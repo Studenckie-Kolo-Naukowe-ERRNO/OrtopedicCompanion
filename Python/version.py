bbad1e4487bca991d5e66ab3ba91d806d0392d99
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2023-11-28 23:58:20 +0100

    basic python setup
    - gitignore
    - changed the project architecture

---
+++ Python/version.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+# https://github.com/ultralytics/ultralytics
+# https://docs.ultralytics.com/tasks/pose/?h=pose
+
+# usually represented as a set of 2D [x, y] or 3D [x, y, visible] coordinates.
+
+# Python >= 3.8
+# PyTorch >= 1.8
